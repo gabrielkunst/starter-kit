2d293a448ce06671130e7ff8624d3e31600f81ca
Gabriel Kunst <gabrielkt.dev@gmail.com>
2024-08-08 19:38:23 -0300

    chore: remove eslint from husky

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-
-module.exports = {
-  ignorePatterns: ["apps/**", "packages/**", "config/**"],
-  extends: ["@starter/eslint-config/library.js"],
-  parser: "@typescript-eslint/parser",
-};
